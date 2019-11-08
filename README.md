916df2e8cce966a96132bea050f09b49133ed397
Zetong Yang <zy891@nyu.edu>
2019-11-09 01:45:35 +0800

    adds .gitignore, adds config.py for keys and passwords

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CSCI-SHU-213-Databases-Final-Project
-A database system for a dummy airline company
+# Airline Website: Vue + Flask + Mysql
+A database system for a hypothetic airline company as the final project for CSCI-SHU 213 Databases.
--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
-import pymysql.cursors
+from config import db, secret_key
 
 #Initialize the app from Flask
 app = Flask(__name__)
-
-#Configure MySQL
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='',
-                       db='blog',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
 
 #Define a route to hello function
 @app.route('/')
@@ -36,7 +28,7 @@ def loginAuth():
 	password = request.form['password']
 
 	#cursor used to send queries
-	cursor = conn.cursor()
+	cursor = db.cursor()
 	#executes query
 	query = 'SELECT * FROM user WHERE username = %s and password = %s'
 	cursor.execute(query, (username, password))
@@ -63,7 +55,7 @@ def registerAuth():
 	password = request.form['password']
 
 	#cursor used to send queries
-	cursor = conn.cursor()
+	cursor = db.cursor()
 	#executes query
 	query = 'SELECT * FROM user WHERE username = %s'
 	cursor.execute(query, (username))
@@ -78,7 +70,7 @@ def registerAuth():
 	else:
 		ins = 'INSERT INTO user VALUES(%s, %s)'
 		cursor.execute(ins, (username, password))
-		conn.commit()
+		db.commit()
 		cursor.close()
 		return render_template('index.html')
 
@@ -86,7 +78,7 @@ def registerAuth():
 def home():
     
     username = session['username']
-    cursor = conn.cursor()
+    cursor = db.cursor()
     query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
     cursor.execute(query, (username))
     data1 = cursor.fetchall() 
@@ -99,11 +91,11 @@ def home():
 @app.route('/post', methods=['GET', 'POST'])
 def post():
 	username = session['username']
-	cursor = conn.cursor()
+	cursor = db.cursor()
 	blog = request.form['blog']
 	query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
 	cursor.execute(query, (blog, username))
-	conn.commit()
+	db.commit()
 	cursor.close()
 	return redirect(url_for('home'))
 
@@ -112,7 +104,8 @@ def logout():
 	session.pop('username')
 	return redirect('/')
 		
-app.secret_key = 'some key that you will never guess'
+app.secret_key = secret_key
+
 #Run the app on localhost port 5000
 #debug = True -> you don't have to restart flask
 #for changes to go through, TURN OFF FOR PRODUCTION
