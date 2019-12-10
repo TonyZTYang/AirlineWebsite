1b129308de61e91ed7f158dca0a856463bde7590
Zetong Yang <zy891@nyu.edu>
2019-12-10 13:15:33 +0800

    completes public search

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 from config import db, secret_key
+from util import fetchall
 
 '''
 # replacement of config.py
@@ -20,17 +21,81 @@ db = pymysql.connect(host='mysql server address',
 #Initialize the app from Flask
 app = Flask(__name__)
 
-#Define a route to hello function
-@app.route('/')
-def hello():
+#Define a route to index
+@app.route('/', methods = ['GET'])
+def index_get():
 	return render_template('index.html')
 
-#Define route for login
+#Add route for public info search
+@app.route('/', methods = ['POST'])
+def public_info_search():
+	# fetch info from form
+	depart_airport = request.form.get('depart_airport')
+	arrive_airport = request.form.get('arrive_airport')
+	depart_date = request.form.get('depart_date')
+	return_date = request.form.get('return_date')
+	depart_city = request.form.get('depart_city')
+	arrive_city = request.form.get('arrive_city')
+	airline_name = request.form.get('airline_name')
+	flight_num = request.form.get('flight_num')
+
+	# check for search scinario
+	if depart_airport:
+		sql = 'select distinct airline_name, flight_number, departure_time , \
+				arrival_time, departure_airport, arrival_airport, \
+				status  from flight where departure_airport = %s and \
+				arrival_airport = %s and DATE(departure_time) = %s'
+		keys = (depart_airport, arrive_airport, depart_date) 
+		result = fetchall(sql,keys)
+		if return_date:
+			sql = 'select distinct airline_name,flight_number,departure_time, \
+				arrival_time, departure_airport, arrival_airport, \
+				status  from flight where departure_airport = %s and \
+				arrival_airport = %s and DATE(departure_time) = %s'
+			keys = (arrive_airport, depart_airport, return_date)
+			return_flight = fetchall(sql, keys)
+			return render_template('index.html',search1=result,
+					search2=return_flight)
+		return render_template('index.html',search1=result)
+	elif depart_city:
+		sql = 'select distinct airline_name,flight_number,departure_time , \
+				arrival_time, departure_airport, arrival_airport, \
+				status  from flight where departure_airport = \
+				(select name from airport where city = %s) and \
+				arrival_airport = (select name from airport where city = %s) \
+				and DATE(departure_time) = %s'
+		keys = (depart_city, arrive_city, depart_date) 
+		result = fetchall(sql,keys)
+		if return_date:
+			sql = 'select distinct airline_name,flight_number,departure_time, \
+				arrival_time, departure_airport, arrival_airport, \
+				status  from flight where departure_airport =  \
+				(select name from airport where city = %s) and \
+				arrival_airport = (select name from airport where city = %s) \
+				and DATE(departure_time) = %s'
+			keys = (arrive_city, depart_city, return_date)
+			return_flight = fetchall(sql, keys)
+			return render_template('index.html',search1=result,
+					search2=return_flight)
+		return render_template('index.html',search1=result)
+	elif airline_name:
+		sql = 'select distinct airline_name,flight_number,departure_time , \
+				arrival_time, departure_airport, arrival_airport, \
+				status  from flight where airline_name = %s and \
+				flight_number = %s \
+				and DATE(departure_time) = %s'
+		keys = (airline_name, flight_num, depart_date) 
+		result = fetchall(sql,keys)
+		return render_template('index.html',search3=result)
+	else:
+		return 'form submission not matching'
+
+#Define route for login type choosing
 @app.route('/login')
 def login():
 	return render_template('login.html')
 
-#Define route for register
+#Define route for register type choosing
 @app.route('/register')
 def register():
 	return render_template('register.html')
